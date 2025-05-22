503708e4a0fbf12e7beee662d749f96d0b75a5ed
stefanarctic <aleluianu09@gmail.com>
2025-05-22 23:55:31 +0300

    Implemented navbar icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Book, FileQuestion, HelpCircle, Home, Layout, ListCheck, ListChecks, Settings, User } from "lucide-react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
@@ -12,11 +13,31 @@ const Navbar = () => {
             <div id="nav-container">
                 <ul id="nav-list">
                     <li>
-                        <a data-href="#home">Acasa</a>
+                        <Link to="/" className="nav-link">
+                            <Home className="nav-icon" />
+                            <span>Acasa</span>
+                        </Link>
+                        <Link to="/probleme" className="nav-link">
+                            <ListCheck className="nav-icon" />
+                            <span>Probleme</span>
+                        </Link>
+                        <Link to="/simulari" className="nav-link">
+                            <Settings className="nav-icon" />
+                            <span>Simulari</span>
+                        </Link>
+                        <Link to="/resurse" className="nav-link">
+                            <Book className="nav-icon" />
+                            <span>Resurse</span>
+                        </Link>
+                        <Link to="/profil" className="nav-link">
+                            <User className="nav-icon" />
+                            <span>Profil</span>
+                        </Link>
+                        {/* <a data-href="#home">Acasa</a>
                         <a data-href="#achievements">Probleme</a>
                         <a data-href="#our-work">Simulari</a>
                         <a data-href="#testimonials">Resurse</a>
-                        <a data-href="#services">Profil</a>
+                        <a data-href="#services">Profil</a> */}
                     </li>
                 </ul>
                 <div id="nav-mobile">
