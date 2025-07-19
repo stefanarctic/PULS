523cfb9066ee997aa2360aa6b9e119ee934343fc
stefanarctic <aleluianu09@gmail.com>
2025-07-20 00:05:39 +0300

    Begin hero responsiveness

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8000
+    port: 8000,
+    allowedHosts: [
+      'localhost',
+      'bd1f94afe421.ngrok-free.app'
+    ]
   },
   resolve: {
     alias: {
